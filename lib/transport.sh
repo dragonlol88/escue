6deb173f5a96549850872e0e20340e7e60b5ce69
suuny <uram24@42maru.com>
2021-08-05 21:39:58 +0900

    Add ssh options and Error handling

--- lib/transport.sh
+++ lib/transport.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
 
+source "./config/globals"
+
+
+parse_options() {
+  # trim 양쪽 space
+  options=$(echo $1 | sed -n -e 's/^\s*// p' | sed -n -e 's/\s*$//p')
+  if [ "$options" != ',' ]; then
+    options="-o $(echo $options | sed -n -e 's/,/ -o /g p')"
+  else
+    options=''
+  fi
+}
+
 Transport()
 {
-  
-  
+
+  host=$1
+  port=$2
+  user=$3
+  identity_file=$4
+  ssh_options=$5
+  cluster=$6
+  node=$7
+
   http_trasport()
   {
     # $1=method
@@ -12,15 +32,87 @@ Transport()
     # $4=data
     # $5=headers
     # Options(timeout)
-    echo "Transport HTTP request using curl" > http_request
 
+    case $1 in
+      -t) echo "Transport HTTP request using curl" > http_request ;;
+       *)
+
+         echo "Wait for develop"
+      ;;
+    esac
   }
-  
-  scp_file_transport()
+
+  ssh_command(){
+
+    # options: ssh options
+    # key=value or key=value,value
+
+    # multiple options
+    # -o key1=value1 -o key2=value2
+
+    local stderr
+    if [ ! -d logs ]; then
+        mkdir logs
+    fi
+    stderr="./logs/$(uuidgen)"
+    command=$1 # requirement parameter
+    shift
+    options="$ssh_options,"$@""
+    parse_options $options
+    if [ -n "$identity_file" ]; then
+      OUTPUT=$(ssh $options -i $identity_file "$user@$host" $command 2>${stderr})
+    else
+      OUTPUT=$(ssh $options "$user@$host" $command 2>${stderr})
+    fi
+    status=$?
+    if [ $status -ne 0  ]; then
+      ERROR=$(sed -n '1, $ p' ${stderr})
+      rm -rf ${stderr}
+      echo "ssh $command is failed  $options"
+      echo "Check the transport error log(escue logs transporterror)"
+      echo "[ `date` ssh_command cluster: $cluster node: $node message: $ERROR]" &>> $TRANSPORTERRORLOGPATH
+      exit 1
+    fi
+    echo "$OUTPUT"
+    return 0
+  }
+
+  scp_transport()
   {
-    echo "Transport file using scp protocol" > scp_file_transfer
-  }
+    local stderr
+    if [ ! -d logs ]; then
+        mkdir logs
+    fi
+    stderr="./logs/$(uuidgen)"
+    case $1 in
+      -t) echo "Transport file using scp protocol" > scp_file_transfer ;;
+       *)
+         source=$1
+         target=$2
+         # source
+         # target directory
+         shift 2
+         options="$ssh_options,"$@""
+         parse_options $options
+         if [ -n "$identity_file" ]; then
+           OUTPUT=$(scp $options -i  $identity_file $source "$user@$host:$target" 2>${stderr})
+         else
+           OUTPUT=$(scp $options $source "$user@$host:$target" 2>${stderr})
+         fi
 
-  echo "HTTP Transport"
+         status=$?
+         if [ $status -ne 0 ]; then
+            ERROR=$(sed -n '1, $ p' ${stderr})
+            rm -rf ${stderr}
+            echo "Transfer $source to ${host}'s $target is failed. $options "
+            echo "Check the transport error log(escue logs transporterror)"
+            echo "[ `date` scp_file_transfer cluster: $cluster node: $node message: $ERROR]" &>> $TRANSPORTERRORLOGPATH
+         fi
+         echo "$OUTPUT"
+         return 0
+      ;;
+    esac
+
+  }
 }
 
