f6cfc812f21d4dc6b1bb1f026f5d616c8e79aff3
suuny <uram24@42maru.com>
2021-08-05 16:13:30 +0900

    Change node meta file system

--- lib/create.sh
+++ lib/create.sh
@@ -4,20 +4,6 @@
 source "./lib/utils.sh"
 source "./config/globals"
 
-questionPath='/home/ec2-user/escue/config/question'
-clusterName="cluster.name"
-nodeName="node.name"
-configPath="config.path"
-HTTPHost="http.host"
-HTTPPort="http.port"
-serverUserName="server.username"
-
-
-configureCluster()
-{
-  moveTo "${cur_loc}/${1}"
-}
-
 createCluster()
 {
   # register cluster
@@ -41,14 +27,14 @@ createCluster()
     echo "Check clusters (escue cluster list) "
     exit 1
   fi
-
-  configureCluster $cluster
+  moveTo "${cur_loc}/$cluster"
   return 0
 
 }
 
 configuredNode()
 {
+
   configs+=(["$clusterName"]="$1")
   configs+=(["$nodeName"]="$2")
   while read question; do
@@ -63,13 +49,13 @@ configuredNode()
     done< <(echo $question | sed 's/\(.\+\)=\(.\+\)/\1\n\2/g')
 
     read -u 1 -p "${q} " input
-
     # replace q to input
-    configs+=( ["${key}"]="${input}" )
+
+    configs+=(["${key}"]="${input}")
 
   # Extract lines from [node] to next [any string]
   # And remove comment and [...] lines
-  done< <(extractLines "\[node\]" "\[.*\]" -e "^#\|\[.*\]" "$questionPath" )
+  done< <(extractLines "\[node\]" "\[.*\]" -e "^#\|\[.*\]\|^[[:space:]]*$" "$questionPath" )
   return 0
 }
 
@@ -100,26 +86,37 @@ createNode()
       echo "${nodename} has been added already, enter other name"
       exit 1
   fi
-
   configuredNode $cluster $nodename
   # save status
   # because status code($1) has been change after if clause
   configureNodeStatus=$?
 
   if [ $configureNodeStatus -eq 0 ]; then
+    moveTo "${cluster_dir}/${nodename}"
+    yml_file="${cluster_dir}/${nodename}/${YMLFILE}"
+    jvm_file="${cluster_dir}/${nodename}/${JVMFILE}"
+    sever_file="${cluster_dir}/${nodename}/${SEVERFILE}"
+
     printf "%s\n" \
-          "[elasticsearch.yml]" \
-          "${clusterName}=${configs[${clusterName}]}"\
-          "${nodeName}=${configs[${nodeName}]}"\
-          "${HTTPHost}=${configs[${HTTPHost}]}"\
-          "${HTTPPort}=${configs[${HTTPPort}]}"\
-          ""\
-          "[syncronize]"\
-          "${configPath}=${configs[${configPath}]}"\
-          ""\
-          "[server]"\
+          "${clusterName}: ${configs[${clusterName}]}"\
+          "${nodeName}: ${configs[${nodeName}]}"\
+          "${nodeRoles}: ${configs[${nodeRoles}]}"\
+          "${dataPath}: ${configs[${dataPath}]}"\
+          "${logsPath}: ${configs[${logsPath}]}"\
+          "${HTTPHost}: ${configs[${HTTPHost}]}"\
+          "${HTTPPort}: ${configs[${HTTPPort}]}"\
+          "${seedHosts}: [${configs[${seedHosts}]}]"\
+          "${initialMasterNodes}: [${configs[${initialMasterNodes}]}]" | sed -e '/^[[:space:]]*$\|.*:\s*$/ d' > $yml_file
+
+    printf "%s\n" \
+          "-Xms${configs[${jvmHeap}]}"\
+          "-Xmx${configs[${jvmHeap}]}"\ > "${cluster_dir}/${nodename}/${JVMFILE}"
+    sed '/^[[:space:]]*$/ d' "${cluster_dir}/${nodename}/${JVMFILE}" | sed -e '/^[[:space:]]*$/ d' > $jvm_file
+    printf "%s\n" \
           "${serverUserName}=${configs[${serverUserName}]}"\
-          > "${cluster_dir}/${nodename}"
+          "${serverHost}=${configs[${serverHost}]}"\
+          "${installPath}=${configs[${installPath}]}"\ | sed -e '/^[[:space:]]*$\|.*=\s*$/ d' > $sever_file
+
     return 0
   fi
 }
