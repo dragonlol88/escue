b423d28602218628eb4118f4b305e8ee90de1af1
suuny <uram24@42maru.com>
2021-08-11 09:10:25 +0900

    Arrange code

--- lib/create.sh
+++ lib/create.sh
@@ -12,40 +12,31 @@ function create_cluster()
   # $1  cluster directory
   # $2  cluster name
 
-  if [ ! -d "${1}" ]; then
-    mkdir -p $1
-  fi
+  [[ ! -d "${1}" ]] && mkdir -p $1
 
   cluster=$2
   cur_loc=$( cd "$1" || exit 1; pwd)
 
   check_for "${cur_loc}/${cluster}"
-  status=$?
+
   # status check
-  if [ $status = 1 ]; then
-    echo "$cluster has been added already"
-    echo "Check clusters (escue cluster list) "
-    exit 1
-  fi
-  move_to "${cur_loc}/$cluster"
-  return 0
+  STATUS=$?
+  msg="$cluster has been added already\nCheck clusters (escue cluster list) "
+  [[ $STATUS -ne 0 ]] && echo -e $msg && return 1
+  move_to "${cur_loc}/$cluster" && return 0 || return 1
 
 }
 
 function configure_node()
 {
 
-  configs+=(["$clusterName"]="$1")
-  configs+=(["$nodeName"]="$2")
+  configs+=(["yml_$cluster_name"]="$1")
+  configs+=(["yml_$node_name"]="$2")
   while read question; do
     count=0
     while read line; do
-        if [ $count -eq 0 ]; then
-          key=$line
-        else
-          q=$line
-        fi
-        ((count=count+1))
+      [[ $count -eq 0 ]] && key=$line || q=$line
+      ((count++))
     done< <(echo $question | sed 's/\(.\+\)=\(.\+\)/\1\n\2/g')
 
     read -u 1 -p "${q} " input
@@ -55,7 +46,8 @@ function configure_node()
 
   # Extract lines from [node] to next [any string]
   # And remove comment and [...] lines
-  done< <(extract_lines "\[node\]" "\[.*\]" -e "^#\|\[.*\]\|^[[:space:]]*$" "$questionPath" )
+  done< <(extract_lines "\[node\]" "\[.*\]" -e "^#\|\[.*\]\|^[[:space:]]*$" "$QUSTION_PATH" )
+
   return 0
 }
 
@@ -65,58 +57,67 @@ function create_node()
 
   declare -A configs
   cluster=$1
-  cluster_dir="${CLUSTER_DIR}/${cluster}"
   nodename=$2
+  cluster_dir="$CLUSTER_DIR/$cluster"
 
   # Check cluster if exist
   check_for "${cluster_dir}"
   status=$?
   # status check
-  if [ $status = 0 ]; then
-      echo "${cluster} cluster does not exit, first create cluster"
-      echo "Command: escue cluster create ${cluster}"
-      exit 1
-  fi
+  [[ $status = 0 ]] && echo "${cluster} cluster does not exit, first create cluster\n" && exit 1
 
   # Check node if exist
   check_for "${cluster_dir}/${nodename}"
   status=$?
   # status check
-  if [ $status = 1 ]; then
-      echo "${nodename} has been added already, enter other name"
-      exit 1
-  fi
+  [[ $status = 1 ]] && echo "${nodename} has been added already, enter other name" && exit 1
+
   configure_node $cluster $nodename
+
   # save status
   # because status code($1) has been change after if clause
   configureNodeStatus=$?
-
   if [ $configureNodeStatus -eq 0 ]; then
-    move_to "${cluster_dir}/${nodename}"
-    yml_file="${cluster_dir}/${nodename}/${YMLFILE}"
-    jvm_file="${cluster_dir}/${nodename}/${JVMFILE}"
-    sever_file="${cluster_dir}/${nodename}/${SEVERFILE}"
+    base="${cluster_dir}/${nodename}"
+    mkdir -p "$base/$JVMDIR"
 
-    printf "%s\n" \
-          "${clusterName}: ${configs[${clusterName}]}"\
-          "${nodeName}: ${configs[${nodeName}]}"\
-          "${nodeRoles}: ${configs[${nodeRoles}]}"\
-          "${dataPath}: ${configs[${dataPath}]}"\
-          "${logsPath}: ${configs[${logsPath}]}"\
-          "${HTTPHost}: ${configs[${HTTPHost}]}"\
-          "${HTTPPort}: ${configs[${HTTPPort}]}"\
-          "${seedHosts}: [${configs[${seedHosts}]}]"\
-          "${initialMasterNodes}: [${configs[${initialMasterNodes}]}]" | sed -e '/^[[:space:]]*$\|.*:\s*$/ d' > $yml_file
+    yml_file="$base/$YMLFILE"
+    jvm_file="$base/$JVMDIR/$JVMFILE"
+    server_file="$base/$SEVERFILE"
 
-    printf "%s\n" \
-          "-Xms${configs[${jvmHeap}]}"\
-          "-Xmx${configs[${jvmHeap}]}"\ > "${cluster_dir}/${nodename}/${JVMFILE}"
-    sed '/^[[:space:]]*$/ d' "${cluster_dir}/${nodename}/${JVMFILE}" | sed -e '/^[[:space:]]*$/ d' > $jvm_file
-    printf "%s\n" \
-          "${serverUserName}=${configs[${serverUserName}]}"\
-          "${serverHost}=${configs[${serverHost}]}"\
-          "${installPath}=${configs[${installPath}]}"\ | sed -e '/^[[:space:]]*$\|.*=\s*$/ d' > $sever_file
-
+    yml_writer && jvm_writer && server_writer
     return 0
   fi
+}
+
+
+function _format_file() {
+  prefix=$1
+  for key in "${!configs[@]}"; do
+
+    if [[ -n $(echo $key | grep $prefix.\\+$) ]]; then
+      nky=${key/$prefix/}
+      pairs+=("${nky}: ${configs[${key}]}")
+    fi
+  done
+}
+
+function yml_writer() {
+  declare -a pairs
+  _format_file yml_
+  printf "%s\n" "${pairs[@]}" | sed -e '/^[[:space:]]*$\|.*:\s*$/ d' > $yml_file
+
+}
+
+function jvm_writer() {
+  declare -a pairs
+  _format_file jvm_
+  printf "%s\n" "${pairs[@]}" | sed -e '/^[[:space:]]*$/ d' > $jvm_file
+}
+
+function server_writer() {
+  declare -a pairs
+  _format_file sv_
+  printf "%s\n" "${pairs[@]}" | sed -e '/^[[:space:]]*$\|.*:\s*$/ d' > $server_file
+
 }
