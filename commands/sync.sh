1579c0dafede17a598ed29945b124560a8865b07
suuny <uram24@42maru.com>
2021-08-20 10:59:45 +0900

    remove plugin sync and move cluster install

--- commands/sync.sh
+++ commands/sync.sh
@@ -15,25 +15,25 @@ PARENT_PATH=$( cd "$(dirname "$0")" && cd .. || exit 1; pwd )
 source "${PARENT_PATH}/lib/sync.sh"
 
 function usage() {
-  cat "${PARENT_PATH}/usage/node"
+  cat "${PARENT_PATH}/usage/sync"
   exit 1
 }
 
 COMMAND=$1; shift
 
-PARSED_ARGUMENTS=$(getopt -a -n "escue sync-file" -o aejo:i:f:n:t: --long file:,node:,target: -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n "escue sync" -o aejo:i:s:n:t: --long source:,node:,target: -- "$@")
 VALID_ARGUMENTS=$?
 
 eval set -- "$PARSED_ARGUMENTS"
 
 while : ; do
     case $1 in
-      -f|--file  ) FILE=$2         ; shift 2 ;;
-      -t|--target) TARGET=$2       ; shift 2 ;;
-      -i         ) INDENTY_FILE=$2 ; shift 2 ;;
-      -o         ) SSHOPTIONS=$2   ; shift 2 ;;
-      -n| --node ) NODE=$2         ; shift 2 ;;
-      --) shift; break ;;
+      -s|--source   ) SOURCE=$2       ; shift 2 ;;
+      -t|--target   ) TARGET=$2       ; shift 2 ;;
+      -i            ) INDENTY_FILE=$2 ; shift 2 ;;
+      -o            ) SSHOPTIONS=$2   ; shift 2 ;;
+      -n| --node    ) NODE=$2         ; shift 2 ;;
+      --) shift; break;;
       *)
         echo "Unexpected option: $1 - this should not happen."
         usage ;;
@@ -47,14 +47,12 @@ CLUSTER="$@"
 [[ $COMMAND = 'yml' ]] || [[ $COMMAND = 'jvm' ]] && [[ -z $NODE ]] && usage
 [[ $COMMAND = 'yml' ]] || [[ $COMMAND = 'jvm' ]] && [[ -n $TARGET ]] && usage
 [[ $COMMAND = 'yml' ]] || [[ $COMMAND = 'jvm' ]] && [[ -n $FILE ]] && usage
-[[ $COMMAND = 'ana' ]]  && [[ -n $NODE ]] && usage
-
+[[ $COMMAND = 'ana' ]] && [[ -n $NODE ]] && usage
 
 case $COMMAND in
   yml        ) sync_yml $CLUSTER $NODE "$INDENTY_FILE" "$SSHOPTIONS" ;;
   jvm        ) sync_jvm $CLUSTER $NODE "$INDENTY_FILE" "$SSHOPTIONS" ;;
-  ana        ) sync_ana $CLUSTER $FILE "$TARGET" "$INDENTY_FILE" "$SSHOPTIONS" ;;
-  plugins    ) sync_plugin $CLUSTER $FILE "$TARGET" "$INDENTY_FILE" "$SSHOPTIONS" ;;
+  ana        ) sync_ana $CLUSTER $SOURCE "$TARGET" "$INDENTY_FILE" "$SSHOPTIONS" ;;
   -h | --help) usage ;;
   *) usage ;;
 esac
